1d18741944f208b8f4a57e1ae04fc3b4fc38bdd7
SanPeter <stpierre.eric@gmail.com>
2024-05-27 20:37:10 -0400

    Update test component to use TypeScript

--- src/components/HelloWorld.svelte
+++ src/components/HelloWorld.svelte
@@ -1,9 +1,9 @@
-<script>
+<script lang="ts">
     import dotenv from 'dotenv';
 
     dotenv.config();
 
-    let message = process.env.MESSAGE;
+    let message: string = process.env.MESSAGE || '';
 </script>
 
 <h1>Hello {message}!</h1>
