cdc4f504f63c1017ed86c3d48f6bae8d49e70301
SanPeter <stpierre.eric@gmail.com>
2024-05-28 15:15:54 -0400

    Added dynamic pages examples

---
+++ src/components/ReferencePage.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+    export let reference : { title: string, slug: string, content: string };
+</script>
+
+<p>Reference content goes here this Svelte component...</p>
+<p>{reference.content}</p>
