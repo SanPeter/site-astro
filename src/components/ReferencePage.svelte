bd45aea0e7c71ee08d8e5249f943e3e1d37b6715
SanPeter <stpierre.eric@gmail.com>
2024-05-30 15:03:04 -0400

    Remove test files

--- src/components/ReferencePage.svelte
+++
@@ -1,6 +0,0 @@
-<script lang="ts">
-    export let reference : Reference;
-</script>
-
-<p>Reference content goes here this Svelte component...</p>
-<p>{reference.content}</p>
--- src/types/articlepage.d.ts
+++ src/types/articlepage.d.ts
@@ -5,7 +5,6 @@ export interface IArticlePage {
             items: IArticlePageItem[];
         }
     }
-
 };
 
 export interface IArticlePageItem {
